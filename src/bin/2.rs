4a249e44e5eabcc749c19f9dbfbbaffd37d6dd57
iamma00 <mahmoodsam53@gmail.com>
2024-10-05 18:24:12 +0530

    upd

---
+++ src/bin/2.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let sum = 2 + 2;
+    let sub = 10 - 5;
+    let mul = 5 * 5;
+    let div = 10 / 2;
+    let rem = 6 % 2;
+    println!(
+        "sum:{}, sub:{}, mul:{}, div:{}, rem:{} ",
+        sum, sub, mul, div, rem
+    );
+}
